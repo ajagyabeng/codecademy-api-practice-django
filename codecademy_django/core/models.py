be931208a6cfd0f0e2c4f1b95fdec7561a76e79d
ajagyabeng <ajagyabeng@gmail.com>
2023-04-05 12:54:54 +0000

    Refactor code

--- codecademy_django/core/models.py
+++ codecademy_django/core/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Photo(models.Model):
+    image_url = models.CharField(max_length=250, null=True)
+    author = models.ForeignKey(
+        User, on_delete=models.PROTECT, related_name="photos")
+    created = models.DateTimeField(auto_now_add=True)
+    venue = models.ForeignKey(
+        "Venue", on_delete=models.CASCADE, null=True, related_name="photos")
+
+    def __str__(self):
+        return f"{self.image_url} {self.author.first_name} {self.author.last_name}"
 
 
 class Venue(models.Model):
@@ -10,18 +20,8 @@ class Venue(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
         User, on_delete=models.PROTECT, related_name="venues")
+    photo = models.ForeignKey(
+        Photo, on_delete=models.CASCADE, related_name="venues", default=7)
 
     def __str__(self):
         return f"{self.name} {self.address}"
-
-
-class Photo(models.Model):
-    image_url = models.CharField(max_length=250, null=True)
-    author = models.ForeignKey(
-        User, on_delete=models.PROTECT, related_name="photos")
-    venue = models.ForeignKey(
-        Venue, on_delete=models.CASCADE, related_name="photos", null=True)
-    created = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.image_url} {self.author.first_name} {self.author.last_name} {self.venue.name} {self.venue.address}"
