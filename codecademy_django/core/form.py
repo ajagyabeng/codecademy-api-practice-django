a3b35ee29fd7ee361c98fc4ff9849a7cba0c4cb0
ajagyabeng <ajagyabeng@gmail.com>
2023-04-05 12:54:17 +0000

    Adjust form fields

--- codecademy_django/core/form.py
+++ codecademy_django/core/form.py
@@ -5,5 +5,4 @@ from django.contrib.auth.models import User
 class MyUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ("first_name", "last_name",
-                  "username", "passsword1", "password2")
+        fields = ["username", "password1", "password2"]
