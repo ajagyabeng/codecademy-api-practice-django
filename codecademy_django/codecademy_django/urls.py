6c7b658931ddcda9bbe37543aea45f98dba689b4
ajagyabeng <ajagyabeng@gmail.com>
2023-04-05 12:52:51 +0000

    Rename router for API

--- codecademy_django/codecademy_django/urls.py
+++ codecademy_django/codecademy_django/urls.py
@@ -4,14 +4,15 @@ from django.urls import path, include
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
 
-from core import views
+from core import views, auth
 
-router = routers.SimpleRouter()
-router.register(r'venues', views.VenueAPIView, basename='venue')
-router.register(r'photos', views.PhotoAPIView, basename='photo')
+api_router = routers.SimpleRouter()
+api_router.register(r'venues', views.VenueAPIView, basename='venue')
+api_router.register(r'photos', views.PhotoAPIView, basename='photo')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/login/', obtain_auth_token, name="obtain_auth_token"),
-    path('api/', include(router.urls))
+    path('api/register/', auth.register_user, name="register"),
+    path('api/', include(api_router.urls)),
 ]
