e5d09ad0c226b39240b00578d72544652d1e85f1
ajagyabeng <ajagyabeng@gmail.com>
2023-04-07 17:03:13 +0000

    Remove venue FK on photo, add manytomany FK on ven

--- codecademy_django/core/models.py
+++ codecademy_django/core/models.py
@@ -3,12 +3,11 @@ from django.contrib.auth.models import User
 
 
 class Photo(models.Model):
+    venue_name = models.CharField(max_length=100)
     image_url = models.CharField(max_length=250, null=True)
     author = models.ForeignKey(
         User, on_delete=models.PROTECT, related_name="photos")
     created = models.DateTimeField(auto_now_add=True)
-    venue = models.ForeignKey(
-        "Venue", on_delete=models.CASCADE, null=True, related_name="photos")
 
     def __str__(self):
         return f"{self.image_url} {self.author.first_name} {self.author.last_name}"
@@ -20,8 +19,8 @@ class Venue(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
         User, on_delete=models.PROTECT, related_name="venues")
-    photo = models.ForeignKey(
-        Photo, on_delete=models.CASCADE, related_name="venues", default=7)
+    photo = models.ManyToManyField(
+        Photo, related_name="venues", blank=True)
 
     def __str__(self):
         return f"{self.name} {self.address}"
